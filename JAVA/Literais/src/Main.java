9cb70e16e523e981aafb86b1c68656f0a459e64a
MarcosPaulo62 <markossylva62@gmail.com>
2023-01-25 10:24:53 -0300

    Aula Literais finalizada - Java Fundamentos

---
+++ JAVA/Literais/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int numero;
+        String cadeiaCaracteres;
+        char caracter;
+        boolean certoOuErrado;
+
+        numero = 10;
+        cadeiaCaracteres = "Start By Capgemini - 2023";
+        caracter = 'T';
+        certoOuErrado = true;
+
+        System.out.println("O valor de numero é : " + numero);
+        System.out.println("O valor de cadeiaCaracteres é : " + cadeiaCaracteres);
+        System.out.println("O valor de caracter é : " + caracter);
+        System.out.println("O valor de certoOuErrado é : " + certoOuErrado);
+    }
+}
