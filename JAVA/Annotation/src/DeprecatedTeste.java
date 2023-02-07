b64ac4e782d2254c1f66295994a6a94af5e6da16
MarcosPaulo62 <markossylva62@gmail.com>
2023-02-07 18:36:07 -0300

    Aula Anotações finalizada - Linguagem de Programação em JAVA

---
+++ JAVA/Annotation/src/DeprecatedTeste.java
@@ -0,0 +1,10 @@
+public class DeprecatedTeste {
+    void visualizar(){
+        System.out.println("Método visualização");
+    }
+
+    @Deprecated
+    void exibir(){
+        System.out.println("Método exibir é deprecated (descontinuado)");
+    }
+}
---
+++ JAVA/Annotation/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        DeprecatedTeste dep = new DeprecatedTeste();
+        dep.visualizar();
+        dep.exibir(); // Dá pra usar, mas não é garantido/confiável
+    }
+}
